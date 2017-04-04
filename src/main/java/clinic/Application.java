e717957f7a0918c09aced7d1a6e88da96b644241
Wesley Rocha <wesleyallanrocha@gmail.com>
2017-04-04 10:46:09 -0300

    Continuando a implementação do spring security;

--- src/main/java/clinic/Application.java
+++ src/main/java/clinic/Application.java
@@ -1,10 +1,25 @@
 package clinic;
 
+import java.security.Principal;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@EnableAuthorizationServer
+@EnableResourceServer
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+@RestController
 public class Application {
+    
+    @GetMapping("/user")
+    public Principal user(Principal user) {
+        return user;
+    }
     
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
--- src/main/java/clinic/paciente/PacienteRepository.java
+++ src/main/java/clinic/paciente/PacienteRepository.java
@@ -2,6 +2,7 @@ package clinic.paciente;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.security.access.annotation.Secured;
 
 /**
  *
