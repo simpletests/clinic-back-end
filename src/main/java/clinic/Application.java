2384e19ea538a6bda2e6faed509b231d5adbeb79
Wesley Rocha <wesleyallanrocha@gmail.com>
2017-04-04 17:22:10 -0300

    Terminado a implementação simples do spring security;

--- src/main/java/clinic/Application.java
+++ src/main/java/clinic/Application.java
@@ -4,6 +4,7 @@ import java.security.Principal;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 @EnableAuthorizationServer
 @EnableResourceServer
 @EnableGlobalMethodSecurity(prePostEnabled = true)
+@EnableWebSecurity
 @RestController
 public class Application {
     
