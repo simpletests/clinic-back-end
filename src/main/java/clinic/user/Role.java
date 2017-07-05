8f1a0d1e3e968e8175c18d0a2e86a75770efc84e
Acer01 <Acer01@ailogdev03>
2017-07-05 16:45:09 -0300

    Alteração no application.properties

--- src/main/java/clinic/user/Role.java
+++ src/main/java/clinic/user/Role.java
@@ -1,11 +1,5 @@
 package clinic.user;
 
-import javax.persistence.Entity;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
 /**
  *
  * @author wesley
